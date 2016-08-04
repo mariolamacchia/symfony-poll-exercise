ef5e87dae32e4de0c4d461683757d981b61d2de4
Mario Lamacchia <mario@appstrakt.com>
2016-08-04 21:50:05 +0200

    Create poll list view

--- src/PollBundle/Controller/DefaultController.php
+++ src/PollBundle/Controller/DefaultController.php
@@ -4,14 +4,36 @@ namespace PollBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use PollBundle\Entity\Poll;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/polls")
      */
-    // public function indexAction()
-    // {
-    //     return $this->render('PollBundle:Default:index.html.twig');
-    // }
+    public function indexAction()
+    {
+        $repository = $this->getDoctrine()
+            ->getRepository('PollBundle:Poll');
+
+        $openPolls = $repository
+            ->createQueryBuilder('p')
+            ->where('p.startDate >= :date')
+            ->where('p.endDate < :date')
+            ->setParameter(':date', date('Y-m-d'))
+            ->getQuery()
+            ->getResult();
+
+        $closedPolls = $repository
+            ->createQueryBuilder('p')
+            ->where('p.endDate >= :date')
+            ->setParameter(':date', date('Y-m-d'))
+            ->getQuery()
+            ->getResult();
+
+        return $this->render('PollBundle:Default:index.html.twig', array (
+            "openPolls"   => $openPolls,
+            "closedPolls" => $closedPolls
+        ));
+    }
 }
